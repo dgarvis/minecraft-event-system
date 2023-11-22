9261014cc2dee270c51fbac7fc16f79711f111c5
Dylan Garvis <dylan@garvis.dev>
2023-11-22 11:41:49 -0500

    Added orginal design doc and then also added shell code for each plugin. Have not tested to see if they load yet.

---
+++ mces-bungee/src/main/java/dev/garvis/mcesbungee/MCESBungeePlugin.java
@@ -0,0 +1,11 @@
+package dev.garvis.mcesbungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class MCESBungeePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+	getLogger().info("onEnable");
+    }
+}
